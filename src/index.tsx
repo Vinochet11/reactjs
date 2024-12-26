f70f127d9ebaee3529dd6a15f9d41bda3efaaa0b
Vinochet11 <jleon2005f@gmail.com>
2024-12-25 23:19:01 -0300

    ss

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-/>
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
